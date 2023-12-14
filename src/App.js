e4fbf246734fa6a043ad01dce0f35abc5c3c19bc
T-queen <142670502+T-queen@users.noreply.github.com>
2023-12-14 10:19:30 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     <h1>Weather App</h1> 
+      <div className="container">
+      <h1>Weather App</h1> 
       <footer>
-        This project was coded by <a href="https://www.linkedin.com/in/taiwo-oresanya-b00209135/" target="_blank" rel="noreferrer">Taiwo Oresanya</a> and is {""}
-      <a href="https://github.com/T-queen/react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub.</a>
+        This project was coded by <a href="https://www.linkedin.com/in/taiwo-oresanya-b00209135/" target="_blank" rel="noreferrer">Taiwo Oresanya</a>{""} and is {""}
+      <a href="https://github.com/T-queen/react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
       </footer>
+      </div>
+    
       
     </div>
   );
