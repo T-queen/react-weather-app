7a79228f8e671bac2caf9c9ee7d51a9fee5976a3
T-queen <142670502+T-queen@users.noreply.github.com>
2023-12-17 12:26:47 +0100

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1> 
+      <Weather /> 
       <footer>
         This project was coded by <a href="https://www.linkedin.com/in/taiwo-oresanya-b00209135/" target="_blank" rel="noreferrer">Taiwo Oresanya</a>{""} and is {""}
       <a href="https://github.com/T-queen/react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
